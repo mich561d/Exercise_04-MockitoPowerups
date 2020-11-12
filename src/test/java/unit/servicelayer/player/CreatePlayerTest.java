cf3a0319a5d1df7da9861d25de993eb8d7dd655f
Michael Pedersen <mich561d@hotmail.com>
2020-11-12 08:38:16 +0100

    Refactored and updated readme

--- src/test/java/unit/servicelayer/player/CreatePlayerTest.java
+++ src/test/java/unit/servicelayer/player/CreatePlayerTest.java
@@ -51,7 +51,7 @@ public class CreatePlayerTest {
             // Act
             playerService.createPlayer(name);
         });
-        verify(storageMock, times(0))
+        verify(storageMock, never())
                 .createPlayer(argThat(x -> x.getName().equals(name)));
     }
 }
